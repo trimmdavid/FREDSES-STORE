167f05f816aee22a63a354d0e28bd3d217b393e5
trimmdavid <61831387+trimmdavid@users.noreply.github.com>
2022-06-07 23:49:27 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+//import Register from "./pages/Register";
+//import Cart from "./pages/Cart";
+ //import Login from "./pages/Login";
+//import Register from "./pages/Register";
+//import Product from "./pages/Product";
+//import ProductList from "./pages/ProductList";
+//import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+
+
+//import Home from "./pages/Home";
+
+//import Home from "./pages/Home";
+//import Product from "./pages/Product";
+
+
+const App = () => {
+  return <Home/>;
+};
+
+export default App; 
---
+++ src/data.jsx
@@ -0,0 +1,76 @@
+export const sliderItems = [
+   {
+     idl:1,
+     img:"https://images.pexels.com/photos/4226835/pexels-photo-4226835.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+     title:"SUMMER SALE",
+     desc:"DON'T COMPROMISE YOUR STYLE! GET FLAT 30% OFF NEW ARRIVALS.",
+     bg:"f5fafd",
+},
+{
+    id:2,
+    img:"https://images.pexels.com/photos/11086909/pexels-photo-11086909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title:"WEDDING COLLECTION",
+    desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg:"fcf1ed",
+},
+{
+    id: 3,
+    img: "https://images.pexels.com/photos/10919896/pexels-photo-10919896.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "ACCESSORIES LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },  
+
+];
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/7899932/pexels-photo-7899932.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "SHIRT STYLE!",
+  },
+  {
+    id: 2,
+    img: "https://www.herald.ng/wp-content/uploads/2018/08/7507925_img20180802162401861_jpegc3d14a021a30d2d142b7e70537af433f.jpeg",
+    title: "AGBADA",
+  },
+  {
+    id: 3,
+    img: "http://cdn.shopify.com/s/files/1/0309/5068/6764/collections/african-jackets-and-coats-114277_grande.jpg?v=1613557042",
+    title: "LIGHT JACKETS",
+  },
+];
+
+export const popularProducts = [
+  {
+    id:1,
+    img:"https://ae01.alicdn.com/kf/H179f6c845fcf489295a8a073dbf849205/African-Shirt-Men-Summer-New-Long-Sleeve-Shirt-Africa-Turquoise-Gold-Printed-Collar-Shirt-Dashiki-Succunct.jpg_Q90.jpg_.webp",
+  },
+  {
+    id:2,
+    img:"https://www.kingleoapparels.com/wp-content/uploads/2021/03/official_packsoncouture_131347177_391596152293375_3560206209988135831_n.jpg",
+  },
+  {
+    id:3,
+    img:"https://images.yaoota.com/__FlGpqI7jNJhvmhrdVYzHtEIcA=/trim/yaootaweb-production-ng/media/crawledproductimages/a0dbf44e48dca38858078630f7cce2062a4c5f90.jpg",
+  },
+  {
+    id:4,
+    img:"https://pictures-nigeria.jijistatic.com/60689409_ofidy-african-women-kaftan-printed-dress-dashiki-orange-v1_1044x1236.jpg",
+  },
+  {
+    id:5,
+    img:"https://cdn.superbee.me/wp-content/uploads/2021/06/reusable-patchwork-tote-bag.jpg",
+  },
+  {
+    id:6,
+    img:"https://africaimports.com/images/thumbs/0033409_kente-kufi-hat-style-1_1920.jpeg",
+  },
+  {
+    id:7,
+    img:"https://m.media-amazon.com/images/I/71wBaIBvXXL._MCnd_AC_UL320_.jpg",
+  },
+  {
+    id:8,
+    img:"https://m.media-amazon.com/images/I/81QkEi7bTAS._MCnd_AC_UL320_.jpg",
+  },
+] 
